8d22e4a1f77e07b8d1c3db13c9c042d3146a5652
Sebastian Blank <blank@data-factory.net>
2020-09-10 17:33:14 +0200

    Compatibility with Symfony 3

--- src/Instance.php
+++ src/Instance.php
@@ -2,13 +2,25 @@
 
 namespace SourceBroker\DeployerInstance;
 
+use Symfony\Component\Dotenv\Dotenv;
+
 class Instance
 {
     public function getLocalInstance()
     {
         if (getenv('INSTANCE') === false) {
             $configFile = getcwd() . '/.env';
-            (new \Symfony\Component\Dotenv\Dotenv())->loadEnv($configFile);
+            if (file_exists($configFile)) {
+                $dotEnv = new Dotenv();
+                if (method_exists($dotEnv, 'loadEnv')) {
+                    // Symfony => 4.2 style
+                    $dotEnv->loadEnv($configFile);
+                } else {
+                    $dotEnv->load($configFile);
+                }
+            } else {
+                throw new \Exception('Missing file "' . $configFile . '"', 1500717945887);
+            }
             if (getenv('INSTANCE') === false) {
                 throw new \Exception('INSTANCE var is no set. Please
             set one of them with the name of INSTANCE which should corresponds to host() name.', 1500717953824);
